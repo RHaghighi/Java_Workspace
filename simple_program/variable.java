eb4326ee3de3266909764343efb4380136e0b40e
reza <reza@reza-TP-T440s>
2017-07-14 10:03:58 +0800

    added revised file

--- simple_program/variable.java
+++ simple_program/variable.java
@@ -4,7 +4,7 @@ class variable{
 	public static void main(String args[]){
 		double tuna;
 		tuna = 5.28;
-		System.out.println(tuna+3);
+		System.out.println(tuna+4);
 		
 		//Scanner a = new Scanner(System.in);
 		//System.out.println(a.nextLine());
